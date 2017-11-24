28a1bf137d015556d42d783e9a5c7347b5590378
Matan Kushner <matchai@me.com>
2017-11-24 10:42:45 -0500

    Add descriptions for aliases

--- fish/alias.fish
+++ fish/alias.fish
@@ -1,24 +1,25 @@
-# hub
-alias git="hub"
-alias g="hub"
-
 # directories
 alias ls="exa"
 alias l="exa -la"
 alias ld="ls -ld */"
 
 # git
-abbr -a gs git status
+alias git="hub"
+alias g="git"
+
+abbr -a gs git status -s -b
 abbr -a ga git add
 abbr -a gc git commit
 abbr -a gcm git commit -m
 abbr -a gp git push
 abbr -a gl git l
+abbr -a gd git diff
 
 # dotbot
 alias db="git -C ~/.dotfiles"
 
 # misc
-alias mkdir="mkdir -p"
-alias vim="nvim"
-alias c="pygmentize -g"
+alias mkdir="mkdir -p"      # Make folder creation recursive
+alias vim="nvim"            # Use Neovim instead of vim
+alias c="pygmentize -g"     # Colored cat
+alias reload="exec fish"    # Reload fish configs
