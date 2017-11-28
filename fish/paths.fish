0633f3f8b9f48d24b30f9b384a37c13d9cc90865
Matan Kushner <matchai@me.com>
2017-11-27 23:36:46 -0500

    Configure Go

--- fish/paths.fish
+++ fish/paths.fish
@@ -1,4 +1,7 @@
 if [ -e ~/dev/go ]
   set -gx GOPATH ~/dev/go
+  set -gx PATH $GOBIN $PATH
+  set -gx GOBIN ~/dev/go/bin
   set -gx GOROOT /usr/local/opt/go/libexec 
+  set -gx PATH $GOROOT/bin $PATH
 end
