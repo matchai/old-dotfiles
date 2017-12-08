648e820881ba2478ef7e5761dc2f23aeaca3f577
Matan Kushner <matchai@me.com>
2017-12-08 13:24:18 -0500

    Add fzf

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,6 @@
 source ~/.config/fish/alias.fish
 source ~/.config/fish/autoenv.fish
 source ~/.config/fish/paths.fish
-source ~/.config/fish/functions.fish
+
+# Disable legacy keybindings for fzf
+set -U FZF_LEGACY_KEYBINDINGS 0
