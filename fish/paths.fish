a2bcfd15592764b68c6039f9349877ad318cc5d6
Matan Kushner <matchai@me.com>
2017-12-09 23:06:49 -0500

    Add comments

--- fish/paths.fish
+++ fish/paths.fish
@@ -1,3 +1,4 @@
+# Configure Go
 if [ -e ~/dev/go ]
   set -gx GOPATH ~/dev/go
   set -gx PATH $GOBIN $PATH
@@ -6,6 +7,7 @@ if [ -e ~/dev/go ]
   set -gx PATH $GOROOT/bin $PATH
 end
 
+# Configure Rust
 if [ -e $HOME/.cargo ]
   set -gx PATH $HOME/.cargo/bin $PATH
 end
