6fbfee6bf1543da3c1db4b6da7d8c3fd822a5f03
Matan Kushner <matchai@me.com>
2018-02-16 11:37:16 -0500

    Clean up git aliases

--- fish/alias.fish
+++ fish/alias.fish
@@ -6,19 +6,22 @@ alias ld="ls -ld */"
 # git
 alias git="hub"
 alias g="git"
-abbr -a gs  git status -s -b
+abbr -a gs  git status -sb
 abbr -a ga  git add
 abbr -a gc  git commit
 abbr -a gcm git commit -m
 abbr -a gca git commit --amend
 abbr -a gcl git clone
+abbr -a gco git checkout
 abbr -a gp  git push
 abbr -a gpl git pull
 abbr -a gl  git l
 abbr -a gd  git diff
 abbr -a gds git diff --staged
-abbr -a gr  git reset
+abbr -a gr  git rebase -i HEAD~15
 abbr -a gf  git fetch
+abbr -a gfc git findcommit
+abbr -a gfm git findmessage
 abbr -a gco git checkout
 
 # dotbot
